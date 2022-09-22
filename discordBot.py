77ca5349ce31fc6a807d455b703fb5b9f0c7ad36
Sonodaart <32108791+Sonodaart@users.noreply.github.com>
2022-09-22 23:14:13 +0200

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -4,7 +4,7 @@ import asyncio
 from yfinance import download
 from requests import get
 from time import time
-from pandas import DataFrame, to_datetime, concat
+from pandas import DataFrame, to_datetime
 from datetime import datetime
 from math import floor
 from agent import AGENT
@@ -27,10 +27,6 @@ azioniCH = 1005245915931615393
 bookCH = 1017463347026858134
 spamCH = 1022115220031811685
 
-# statistiche varie
-bookValues = []
-ohlc = []
-
 def check_time():
 	global Last_update
 	now = datetime.fromtimestamp(time()).strftime("%H:%M:%S").split(":")
@@ -49,68 +45,21 @@ def check_time():
 
 	return False
 
-def check_book_time():
-	global Last_minute
-	now = datetime.fromtimestamp(time()).strftime("%H:%M:%S").split(":")
-	now = int(now[1])
-	if (Last_minute+1)%60 <= now:
-		Last_minute = now
-		return True
-	return False
-
 def get_data(asset):
-	global ohlc
+	"""PERIODO = 5*60 # 5 minuti
 	SPAN = 500 # < 720
+	ORA = int(time())
 
-	"""resp0 = get(f'https://api.binance.com/api/v3/klines?symbol={asset[0]}BUSD&interval=5m&limit={SPAN}')
-	data0 = DataFrame(resp0.json()).rename(columns={0:"Datetime",1:"Open",2:"High",3:"Low",4:"Close",5:"Volume",8:"Trades"})
-	data0["Datetime"] = data0["Datetime"]//1000 # in secondi
+	resp0 = get(f'https://api.kraken.com/0/public/OHLC?pair={asset[0]}EUR&interval=5&since={ORA-SPAN*PERIODO}')
+	data0 = DataFrame(resp0.json()['result'][f'{Agent.currentNameResult[0]}ZEUR'])
+	data0 = data0.rename(columns={0:"Datetime",1:"Open",2:"Low",3:"High",4:"Close",5:"Vwap",6:"Volume",7:"Count"})
+	data0['Datetime'] = to_datetime(data0['Datetime'],unit="s",origin="unix")
 	data0 = data0.set_index("Datetime").sort_index()
-	
-	if type(ohlc) == type([]):
-		ohlc = data0
-	else:
-		ohlc = concat([ohlc, data0]).drop_duplicates(keep='first')
-		ohlc = ohlc.sort_index()
 	"""
-	data0 = download("ETH-USD", start=datetime.now()-timedelta(hours=40), end=datetime.now(), interval="5m", auto_adjust=False, prepost=False).astype(float).sort_index()
+
+	data0 = download("ETH-EUR", start=datetime.now()-timedelta(hours=40), end=datetime.now(), interval="5m", auto_adjust=False, prepost=False).astype(float).sort_index()
 
 	return [data0]
-
-def avg(v):
-	return sum(v)/len(v)
-
-def std(v):
-	if len(v)<=1:
-		return -1
-	medio = avg(v)
-	return pow( sum([(i-medio)**2 for i in v])/(len(v)-1) ,0.5)
-
-def getBook():
-	book = "https://api.binance.com/api/v3/depth?symbol=ETHBUSD&limit=15"
-	return get(book).json()
-
-def printBookStatistics():
-	global bookValues
-	book = getBook()
-	asksSell = book['asks']
-	bidsBuy = book['bids']
-
-	quantitySell = [float(i[1]) for i in asksSell]
-	quantityBuy = [float(i[1]) for i in bidsBuy]
-	priceSell = [float(i[0]) for i in asksSell]
-	priceBuy = [float(i[0]) for i in bidsBuy]
-
-	sommaSell = round(sum(quantitySell),2)
-	sommaBuy = round(sum(quantityBuy),2)
-
-	SellStd = round(std(priceSell),3)
-	BuysStd = round(std(priceBuy),3)
-
-	a = ",".join( [f"[{i[0]},{i[1]}]" for i in asksSell])
-	b = ",".join( [f"[{i[0]},{i[1]}]" for i in bidsBuy])
-	bookValues.append([sommaSell,sommaBuy,SellStd,BuysStd,Agent.get_price(),time()])
-	return f"asksSell:{a}\nbidsBuy:{b}\n{sommaSell}, {sommaBuy}, {SellStd}, {BuysStd}"
 
 def process_data(data):
 	if not Agent.dentro:
@@ -135,8 +84,6 @@ async def on_ready():
 		if SESSION==True:
 			try:
 				await asyncio.sleep(15)
-				if check_book_time():
-					await client.get_channel(bookCH).send(printBookStatistics())
 				if check_time():
 					now = datetime.fromtimestamp(time()).strftime("%H:%M")
 					await client.get_channel(attivitaCH).send(f"Connection check({now}).")
@@ -182,7 +129,7 @@ async def on_message(message):
 		elif message.content=="help" or message.content=="h":
 			await message.channel.send(f"help-h\nversion-v\nshutdown/execute-s\nbalance-b\nstate-c\nforce buy 0\nforce sell\nbook\nenter/exit e")
 		elif message.content=="version" or message.content=="v":
-			await message.channel.send(f"B2.1.3")
+			await message.channel.send(f"K0.0.1")
 		elif message.content=="enter" or message.content=="exit" or message.content=="e":
 			Agent.dentro = not Agent.dentro
 			await message.channel.send(f"Stato corrente aggiornato: dentro={Agent.dentro}")
@@ -207,18 +154,6 @@ async def on_message(message):
 			flag, r = Agent.sell(datetime.fromtimestamp(time()).strftime("%H:%M:%S"),get_data(Agent.currentName),True)
 			if flag:
 				await client.get_channel(transazioniCH).send(r)
-	elif message.channel.id==spamCH:
-		if message.content=="book":
-			m = "["+",".join( [f"[{i[0]},{i[1]},{i[2]},{i[3]},{i[4]},{i[5]}]" for i in bookValues])+"]"
-			for i in range(0,len(m),1995):
-				await message.channel.send(m[i:i+1995])
-		if message.content=="ohlc":
-			m = ohlc[["Open","High","Low","Close","Volume"]].astype(float).round(2).to_string(index=True).split("\n")
-			for i in range(0,len(m),10):
-				await message.channel.send("\n".join(m[i:i+10]))
-	else:
-		#print(message.channel.id)
-		allowed_mentions = discord.AllowedMentions(everyone = True)
-		await message.channel.send(content="@everyone owo", allowed_mentions=allowed_mentions)
+
 
 client.run(os.environ['DISCORD_TOKEN'])
