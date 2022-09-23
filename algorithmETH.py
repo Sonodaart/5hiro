1023d46bfad28d32b953bd726fe53c9bd44031f7
Sonodaart <32108791+Sonodaart@users.noreply.github.com>
2022-09-24 01:37:29 +0200

    Update algorithmETH.py

--- algorithmETH.py
+++ algorithmETH.py
@@ -39,7 +39,8 @@ class AlgorithmETH:
 		rocMACD = 0.3<self.df['rocM'][t] and self.df['rocBreve'][t]<1.5
 		aroonMACD = self.df['aroon_indicator'][t]>50
 		sarM = self.df['psar_di'][t]==False
-		bollingerMACD = self.df['bollinger_wband'][t] > 1.5
+		bollingerMACD = self.df['bollinger_wband'][t] > 1 or True
+		jump = self.df['jump'][t]==False
 		
 		Smacd = self.df[f'EMA{self.Breve}'][t]<self.df[f'EMA{self.Lunga}'][t]
 		SrocMACD = -2<self.df['rocM'][t]<0.3 and self.df['rocLungo'][t]<-0.5 and -2<self.df['rocBreve'][t]<-0.3
@@ -47,7 +48,7 @@ class AlgorithmETH:
 		Sbollinger = self.df['bollinger_pband'][t-10]-self.df['bollinger_pband'][t]<0.1 and self.df['bollinger_pband'][t]>0.3
 		SsarM = self.df['psar_di'][t]==True
 		
-		if macd and rocMACD and aroonMACD and False:
+		if macd and rocMACD and aroonMACD and jump and bollingerMACD:
 			adx = self.df['adx'][t]>40
 			if sarM and adx:
 				self.strategia = "MACD"
@@ -86,6 +87,12 @@ class AlgorithmETH:
 		macd = MACD(self.df['Close'])
 		self.df['macd'] = macd.macd()
 		self.df['signal'] = macd.macd_signal()
+
+		# Differenze
+		self.df['diff'] = self.df['Close']-self.df['Close'].shift(5)
+		v = 40
+		self.df['jump'] = self.df[(self.df['diff']>v) | (self.df['diff'].shift(1)>v) | (self.df['diff'].shift(2)>v) | (self.df['diff'].shift(3)>v) | (self.df['diff'].shift(4)>v) | (self.df['diff'].shift(5)>v) | (self.df['diff'].shift(6)>v) | (self.df['diff'].shift(7)>v) | (self.df['diff'].shift(8)>v) | (self.df['diff'].shift(9)>v)]['diff'].notnull()
+		self.df['jump'].fillna(False,inplace=True)
 
 		# ADX
 		adxI = ADXIndicator(self.df['High'],self.df['Low'],self.df['Close'], self.ADXperiodo, False)
